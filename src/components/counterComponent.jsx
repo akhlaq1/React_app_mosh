80f2da35a9c680766cc37b95570a5cb2a63b0035
akhlaq1 <akhlaq.computer@gmail.com>
2019-02-22 21:35:47 +0500

    adding data to state{}

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 1
+  };
   render() {
     return (
       <React.Fragment>
         <h1>Hello World</h1>
+        <span>{this.countFunc()}</span> <br />
         <button className="btn btn-submit">Count</button>
       </React.Fragment>
     );
+  }
+  countFunc() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : <h1>{count}</h1>;
   }
 }
 
