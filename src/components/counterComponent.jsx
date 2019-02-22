60965d402c3fb11968242fa9a2caf815f720c9b3
akhlaq1 <akhlaq.computer@gmail.com>
2019-02-22 21:57:40 +0500

    adding img

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,20 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
   };
   render() {
     return (
       <React.Fragment>
         <h1>Hello World</h1>
         <span>{this.countFunc()}</span> <br />
-        <button className="btn btn-submit">Count</button>
+        <img className="m-2" src={this.state.imageUrl} alt="Random pic" />
+        <button className="btn btn-secondary btn-sm">Count</button>
       </React.Fragment>
     );
   }
   countFunc() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : <h1>{count}</h1>;
+    return count === 0 ? (
+      <span className="badge badge-primary m-2">Zero</span>
+    ) : (
+      <span className="badge badge-primary m-2">{count}</span>
+    );
   }
 }
 
