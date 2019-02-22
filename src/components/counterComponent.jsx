7ef06ab23da7d1d5a7e1cca4b08bebe0f47ab916
akhlaq1 <akhlaq.computer@gmail.com>
2019-02-22 23:35:28 +0500

    conditional rendering

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,14 +3,11 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
     tags: ["tag1", "tag2", "tag3"]
   };
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClass()}>{this.countFunc()}</span> <br />
-        <button className="btn btn-secondary btn-sm">Count</button>
         <ul>
           {this.state.tags.map(tag => (
             <li key={this.state.tags.indexOf(tag)}>{tag}</li>
@@ -18,16 +15,6 @@ class Counter extends Component {
         </ul>
       </React.Fragment>
     );
-  }
-  getBadgeClass() {
-    let badgeClass = "m-2 badge badge-";
-    badgeClass += this.state.count === 0 ? "warning" : "primary";
-    return badgeClass;
-  }
-
-  countFunc() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : { count };
   }
 }
 
