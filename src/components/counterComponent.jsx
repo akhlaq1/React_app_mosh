5b072fddc0064d7d1fe72904bc956346680e76cb
akhlaq1 <akhlaq.computer@gmail.com>
2019-02-22 22:37:16 +0500

    rendering lists

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,25 +3,31 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200"
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
   };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <span>{this.countFunc()}</span> <br />
-        <img className="m-2" src={this.state.imageUrl} alt="Random pic" />
+        <span className={this.getBadgeClass()}>{this.countFunc()}</span> <br />
         <button className="btn btn-secondary btn-sm">Count</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={this.state.tags.indexOf(tag)}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
+  getBadgeClass() {
+    let badgeClass = "m-2 badge badge-";
+    badgeClass += this.state.count === 0 ? "warning" : "primary";
+    return badgeClass;
+  }
+
   countFunc() {
     const { count } = this.state;
-    return count === 0 ? (
-      <span className="badge badge-primary m-2">Zero</span>
-    ) : (
-      <span className="badge badge-primary m-2">{count}</span>
-    );
+    return count === 0 ? "Zero" : { count };
   }
 }
 
