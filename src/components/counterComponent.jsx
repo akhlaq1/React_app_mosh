a2caaad2ae8092667c2b086459fe18edd2b58d24
akhlaq1 <akhlaq.computer@gmail.com>
2019-02-23 03:01:09 +0500

    conditional rendering

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,18 +3,23 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    // tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
-  render() {
+  forList() {
+    if (this.state.tags.length === 0) {
+      return <h1>The array contains nothing</h1>;
+    }
     return (
-      <React.Fragment>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={this.state.tags.indexOf(tag)}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={this.state.tags.indexOf(tag)}>{tag}</li>
+        ))}
+      </ul>
     );
+  }
+  render() {
+    return <React.Fragment>{this.forList()}</React.Fragment>;
   }
 }
 
