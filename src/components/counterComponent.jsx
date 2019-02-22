57bc5321eb26a1bbd96eb0b0e94f5d97a9562ad8
akhlaq1 <akhlaq.computer@gmail.com>
2019-02-23 03:33:26 +0500

    Method 1 to pass data

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,41 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200",
+    count: 5,
+
     tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClass()}>{this.countFunc()}</span> <br />
         <button
           onClick={this.btn_increment}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={this.state.tags.indexOf(tag)}>{tag}</li>
-          ))}
-        </ul>
       </React.Fragment>
     );
   }
-  btn_increment() {
-    alert("Dont press me idiot!");
-  }
-  getBadgeClass() {
-    let badgeClass = "m-2 badge badge-";
-    badgeClass += this.state.count === 0 ? "warning" : "primary";
-    return badgeClass;
-  }
 
-  countFunc() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : { count };
+  // Method 1 to pass data
+  constructor() {
+    super();
+    this.btn_increment = this.btn_increment.bind(this);
+  }
+  btn_increment() {
+    console.log("Button Pressed", this.state.count);
   }
 }
 
